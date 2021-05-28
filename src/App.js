91f1ca380964ac6de0109d294fab236b7cac3a61
SRRahmaaaan <sydurrahman2699@gmai.com>
2021-05-29 00:32:00 +0600

    working

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react'
+import { Container, AppBar, Typography, Grid, Grow } from "@material-ui/core"
+import {useDispatch} from "react-redux"
+import logo from "./images/logo.svg"
+import Form from './Components/Form/Form'
+import Posts from './Components/Posts/Posts'
+import {getPosts} from "./actions/Posts"
+import useStyles from "./styles"
+
+const App = () => {
+    const classes = useStyles()
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getPosts)
+    }, [dispatch])
+    return (
+        <Container maxWidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center"> Buddy Zone </Typography>
+                <img className={classes.image} src={logo} alt="logo" height="60" />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch">
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+            
+        </Container>
+    )
+}
+
+export default App
